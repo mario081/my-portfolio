7d558875073b8f7a7644c469141120e1ba50cc3b
micaelOliveira <ffa35922@gmail.com>
2025-07-24 13:28:47 -0300

    style: Terminando a estilização

--- src/pages/Home/Hero/hero.tsx
+++ src/pages/Home/Hero/hero.tsx
@@ -3,7 +3,9 @@ import Avatar from "../../../assets/avatar.jpg"
 import DownloadIcon from '@mui/icons-material/Download';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import { Container, Grid, Typography } from "@mui/material"
-import StyledButton from "../../../components/StyledButton/styledButton";
+import StyledButton from "../../../components/StyledButton/StyledButton";
+import { AnimatedBackground } from "../../../components/AnimateBackground/AnimateBackground";
+import Box from '@mui/material/Box';
 
 const Hero = () => {
 
@@ -11,26 +13,33 @@ const Hero = () => {
         <StyledHero>
             <Container maxWidth="lg">
                 <Grid container spacing={2}>
-                    <Grid size={{ xs: 12, md: 4 }}>
-                        <StyledImg src={Avatar} />
+                    <Grid size={{ xs: 12, md: 5 }}>
+                        <Box position="relative">
+                            <Box position="absolute" width={"150%"} top={-100} right={0}>
+                                <AnimatedBackground />
+                            </Box>
+                            <Box position="relative" textAlign="center">
+                                <StyledImg src={Avatar} />
+                            </Box>
+                        </Box>
                     </Grid>
-                    <Grid size={{ xs: 12, md: 8 }}>
+                    <Grid size={{ xs: 12, md: 7 }}>
                         <Typography color="primary.contrastText" variant="h1" textAlign={"center"}>José Mário</Typography>
                         <Typography color="primary.contrastText" variant="h2" textAlign={"center"}>I'm Software Developer</Typography>
                         <Grid container display={"flex"} justifyContent={"center"} spacing={4}>
                             <Grid size={{ xs: 12, md: 4 }} display={"flex"} justifyContent={"center"}>
                                 <StyledButton>
                                     <DownloadIcon />
-                                   <Typography>
-                                    Downloand CV
-                                   </Typography>
+                                    <Typography>
+                                        Downloand CV
+                                    </Typography>
                                 </StyledButton>
                             </Grid>
                             <Grid size={{ xs: 12, md: 4 }} display={"flex"} justifyContent={"center"}>
                                 <StyledButton>
                                     <MailOutlineIcon />
                                     <Typography>
-                                    Contact me
+                                        Contact me
                                     </Typography>
                                 </StyledButton>
                             </Grid>
--- src/pages/Home/Hero/herocss.tsx
+++ src/pages/Home/Hero/herocss.tsx
@@ -4,9 +4,12 @@ import styled from "@emotion/styled";
 export const StyledHero = styled("div")(({ theme }) => ({
     backgroundColor: theme?.palette.primary.main,
     height: "100vh",
+    display: "flex",
+    alignItems: "center",
 }));
 
-export const StyledImg = styled("img")(() => ({
-    width: "100%",
-    borderRadius: "50%"
+export const StyledImg = styled("img")(({theme}) => ({
+    width: "80%",
+    borderRadius: "50%",
+    border: `1px solid ${theme.palette.primary.contrastText}`
 }))
